b325f6acd601e367419d693d730234f152e9131f
Marc Dwyer <marcdwyer@marcs-mbp.attlocal.net>
2020-11-09 00:46:05 -0600

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+export const limit = 8;
+
+function getXY() {
+  const xy = [];
+  const getArray = (n: number): boolean[] => {
+    const result = new Array(n);
+    result.fill(false);
+    return result;
+  };
+  for (let x = 0; x < limit; x++) {
+    xy.push(getArray(limit));
+  }
+  return xy;
+}
 
 function App() {
+  const [xy, setXY] = useState(getXY());
+
+  const setStart = () => {
+    const start = Math.floor((xy.length - 1) / 2);
+    const copy = [...xy];
+    copy[start][1] = true;
+    setXY(copy);
+  };
+  useEffect(() => {
+    setStart();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid">
+        {xy.map((y, x) => {
+          return (
+            <div key={x} className="row">
+              {y.map((y, i) => {
+                return (
+                  <div key={i} className={`cell ${y ? "green" : ""}`}></div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
